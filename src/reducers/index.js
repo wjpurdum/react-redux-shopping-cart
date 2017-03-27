b63530e17f8dc59bae6dc1227bda9c9b9bea00a7
superbuggy <jpqreichard@gmail.com>
2017-03-27 15:22:41 -0400

    move react app into own dir

--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import cart from './cart'
-import { combineReducers } from 'redux'
-
-//stateless components should be defined as functions.
-
-const rootReducer = combineReducers({
-  cart //ES6 short hand for {cart: cart}
-})
